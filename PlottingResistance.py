56de4460a3a5ab7090d9cff88d33b072b3db2a13
Jordan Dowdy <jordan@Jordans-MacBook-Pro-3.local>
2022-09-30 18:38:07 -0400

    updated plotting

--- PlottingResistance.py
+++ PlottingResistance.py
@@ -13,7 +13,7 @@ from unicodedata import name
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-import chardet
+
 
 
 
@@ -21,7 +21,6 @@ import chardet
 def csvFileLocator(csvDir : str) -> "list[str]":                             
 
     pathList=[]
-    nameList=[]
 
     if  not csvDir:
         csvDir = "."
@@ -30,57 +29,70 @@ def csvFileLocator(csvDir : str) -> "list[str]":
         for name in files:
             #if name.endswith(".gz") and name.startswith("lsst_a_"):
             if name.endswith(".csv"):
+                if name.startswith("._"):
+                    continue
                 pathList.append(os.path.join(root, name))
-                #name, fits, gz = name.split(".")
-                name, csv = name.split(".csv")
-                nameList.append(name)
-                print(nameList)
-                print(pathList)
-
-    return pathList, nameList
+    
 
 
-def forcePlotter(fileName : str, x: int):
+
+    return pathList
+
+
+def forcePlotter( x: int):
     csvData = pd.read_csv(csvPaths[x])
     #print(csvData)
     csvData.plot.scatter(x="Time - Force(N)", y="F - Force(N)", alpha=0.5)
     plt.ylabel("Force (newtons)", size=16)
     plt.xlabel("Time (milliseconds)", size=16)
-    print(fileName)
-    plt.show()
+    plotName = namingPlotter(x)
+    plt.title(plotName)
+    plt.savefig(cwd+"/figures/" + plotName + ".png", dpi=1200)
+    plt.close()
 
-def resistancePlotter(fileName : str, x: int):
-    #rawdata = open(csvPaths[x], 'rb').read()
-    #result = chardet.detect(rawdata)
-    #charenc = result['encoding']
-    #print(charenc)
-    print(csvPaths[x])
-    csvResistance = pd.read_csv("/Volumes/USB_DRIVE/Jordan/1layer/A/0.5-2Nsensorresponse9-23-2022.csv", encoding="Windows-1252")
-    print(csvResistance)
+def resistancePlotter(x: int):
+    csvResistance = pd.read_csv(csvPaths[x], encoding="Windows-1252")
+    #print(csvResistance)
     csvResistance.plot.scatter(x="Time - Plot 0", y="Amplitude - Plot 0", alpha=0.5)
-    plt.ylabel("Force (newtons)", size=16)
+    plt.ylabel("Amplitude (ohms)", size=16)
     plt.xlabel("Time (milliseconds)", size=16)
-    print(fileName)
-    plt.show()
+    plotName = namingPlotter(x)
+    plt.title(plotName)
+    plt.savefig(cwd+"/figures/"+ plotName + ".png", dpi=1200)
+    plt.close()
+def namingPlotter(x : int) -> str:
     
+    sensorType = csvPaths[x].replace(mainPath, "")
+    
+    plotName = sensorType.replace("/", "-")
+    plotName = plotName.replace(".csv", "")
+    if plotName[0:1] == "-":
+        plotName = plotName[1:]
+    try:
+        plotName = plotName.replace(".tmp", "")
 
+    except:
+        pass
+    return plotName
 
 def main():
 
     j = 0
-    for i in csvNames:
+    for i in csvPaths:
         print(i)
-        if ("force" in i):
-            forcePlotter(i,j)
+        if (("force" or "Force") in i):
+            forcePlotter(j)
 
-        if ("sensor" in i):
+        if (("sensor" or "Sensor") in i):
             
-            resistancePlotter(i,j)
+            resistancePlotter(j)
 
         j+=1
 
 
 if __name__ == "__main__":
-    csvPaths, csvNames = csvFileLocator("/Volumes/USB_DRIVE/Jordan")
+    cwd = os.getcwd()
+    mainPath = input("Enter path to csv: ")
+    csvPaths = csvFileLocator(mainPath)
     sys.exit(main())
 
