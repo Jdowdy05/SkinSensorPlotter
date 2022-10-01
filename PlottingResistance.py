775cb6b5b3dd9834ffb6478ce151cc49816d9049
Jordan Dowdy <jordan@Jordans-MacBook-Pro-3.local>
2022-10-01 14:44:39 -0400

    fixed naming issue

--- PlottingResistance.py
+++ PlottingResistance.py
@@ -9,7 +9,6 @@ This is used to create good looking plots for our recorded data on sensors :)
 
 """
 import os
-from unicodedata import name
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
@@ -39,29 +38,57 @@ def csvFileLocator(csvDir : str) -> "list[str]":
     return pathList
 
 
-def forcePlotter( x: int):
-    csvData = pd.read_csv(csvPaths[x])
+def forcePlotter(i: int):
+    #plots the data of the force
+    csvData = pd.read_csv(csvPaths[i])
     #print(csvData)
     csvData.plot.scatter(x="Time - Force(N)", y="F - Force(N)", alpha=0.5)
     plt.ylabel("Force (newtons)", size=16)
     plt.xlabel("Time (milliseconds)", size=16)
-    plotName = namingPlotter(x)
+    plotName = namingPlotter(i)
     plt.title(plotName)
-    plt.savefig(cwd+"/figures/" + plotName + ".png", dpi=1200)
+
+    try:
+
+        plt.savefig(cwd+"/figures/" + plotName + ".png", dpi=1200)
+
+    except:
+        pass
+
+    try:
+        plt.savefig(cwd+"\\figures\\" + plotName + ".png", dpi=1200)
+
+    except:
+        print("save error. make sure the figures dir is in the same place as the python file.")
+
     plt.close()
 
-def resistancePlotter(x: int):
-    csvResistance = pd.read_csv(csvPaths[x], encoding="Windows-1252")
+def resistancePlotter(i: int):
+    #plots the data for the skin sensor
+    csvResistance = pd.read_csv(csvPaths[i])
     #print(csvResistance)
     csvResistance.plot.scatter(x="Time - Plot 0", y="Amplitude - Plot 0", alpha=0.5)
     plt.ylabel("Amplitude (ohms)", size=16)
     plt.xlabel("Time (milliseconds)", size=16)
-    plotName = namingPlotter(x)
+    plotName = namingPlotter(i)
     plt.title(plotName)
-    plt.savefig(cwd+"/figures/"+ plotName + ".png", dpi=1200)
+    try:
+
+        plt.savefig(cwd+"/figures/" + plotName + ".png", dpi=1200)
+
+    except:
+        pass
+
+    try:
+        plt.savefig(cwd+"\\figures\\" + plotName + ".png", dpi=1200)
+
+    except:
+        print("save error. make sure the figures dir is in the same place as the python file.")
+
     plt.close()
+
 def namingPlotter(x : int) -> str:
-    
+    #finds the name of file to properly name plot
     sensorType = csvPaths[x].replace(mainPath, "")
     
     plotName = sensorType.replace("/", "-")
@@ -75,19 +102,40 @@ def namingPlotter(x : int) -> str:
         pass
     return plotName
 
+def quadPlotter(i: int):
+    #this is to plot all four of a type onto one plot
+
+    #still needs work
+
+    fig, ax = plt.subplots(2,2)
+    
+    csvResistanceA = pd.read_csv(csvPaths[i-3])
+    aPlot = csvResistanceA.plot.scatter(x="Time - Force(N)", y="F - Force(N)", alpha=0.5)
+    csvResistanceB = pd.read_csv(csvPaths[i-2])
+    bPlot = csvResistanceB.plot.scatter(x="Time - Force(N)", y="F - Force(N)", alpha=0.5)
+    csvResistanceC = pd.read_csv(csvPaths[i-1])
+    cPlot = csvResistanceC.plot.scatter(x="Time - Plot 0", y="Amplitude - Plot 0", alpha=0.5)
+    csvResistanceD = pd.read_csv(csvPaths[i])
+    dPlot = csvResistanceD.plot.scatter(x="Time - Plot 0", y="Amplitude - Plot 0", alpha=0.5)
+    ax[0,0].plot(aPlot)
+    ax[0,1].plot(bPlot)
+    plt.show()
+
 def main():
 
-    j = 0
-    for i in csvPaths:
-        print(i)
-        if (("force" or "Force") in i):
-            forcePlotter(j)
+    i = 0
+    for path in csvPaths:
+        print(path)
+        if (("force" or "Force") in path):
+            forcePlotter(i)
 
-        if (("sensor" or "Sensor") in i):
+        if (("sensor" or "Sensor") in path):
             
-            resistancePlotter(j)
-
-        j+=1
+            resistancePlotter(i)
+        if (i+1)%4 == 0:
+            #quadPlotter(i)
+            pass
+        i+=1
 
 
 if __name__ == "__main__":
